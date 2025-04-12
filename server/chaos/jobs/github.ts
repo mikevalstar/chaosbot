51e13f03c539f6734f831d0575c64b55be4b83e1
Mike Valstar <mike@valstar.dev>
2025-04-12 16:51:24 -0400

    Mr review auto pub
    Auto merged by the agent of chaos

--- server/chaos/jobs/github.ts
+++ server/chaos/jobs/github.ts
@@ -1,6 +1,13 @@
+//import { Octokit } from 'octokit';
 import logger from '../lib/log';
 
+const validUsers = ['mikevalstar'];
+
 export async function githubCheckPRs() {
+  const { Octokit } = await import('octokit');
+
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
   logger.info('Checking PRs');
   const prsResponse = await fetch(
     'https://api.github.com/repos/mikevalstar/chaosbot/pulls?state=open&per_page=100',
@@ -8,7 +15,28 @@ export async function githubCheckPRs() {
 
   const prs = await prsResponse.json();
 
-  console.log(prs);
+  // for each one squash and merge
+  for (const pr of prs) {
+    if (validUsers.includes(pr.user.login)) {
+      logger.info(`Squashing and merging PR ${pr.number}`);
+      try {
+        const data = await octokit.request('PUT /repos/{owner}/{repo}/pulls/{pull_number}/merge', {
+          owner: 'mikevalstar',
+          repo: 'chaosbot',
+          pull_number: pr.number,
+          merge_method: 'squash',
+          commit_title: pr.title,
+          commit_message: 'Auto merged by the agent of chaos',
+          headers: {
+            'X-GitHub-Api-Version': '2022-11-28',
+          },
+        });
+        logger.info(`Squashed and merged PR ${pr.number}: ${data}`);
+      } catch (error) {
+        logger.error(`Error squashing and merging PR ${pr.number}: ${error}`);
+      }
+    }
+  }
 
   setTimeout(githubCheckPRs, 60 * 1000);
 }
